6e98fbbf1714491e45a1af905c9560dbe87e2b5b
Alex Semakin <alexasemakin@gmail.com>
2022-05-16 19:23:42 +0400

    reload project

---
+++ five_chars/five_chars/five_chars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+
+namespace five_chars
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string s = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/alphabet.txt");
+            Console.Write(s);
+        }
+    }
+}
